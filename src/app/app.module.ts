c06aaeacef901917a47c72b1c6a5dc4c49f2da28
Jacopo Salvi <jacopo.salvi.86@gmail.com>
2023-12-09 16:32:44 +0100

    stop&code

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './componenti/navbar/navbar.component';
+import {MatButtonModule} from '@angular/material/button';
+import { EstrattoContoComponent } from './componenti/estratto-conto/estratto-conto.component';
+import { PrelievoComponent } from './componenti/prelievo/prelievo.component';
+import { DepositoComponent } from './componenti/deposito/deposito.component';
+import { DepositaContantiComponent } from './componenti/deposito/deposita-contanti/deposita-contanti.component';
+import { DepositaAssegniComponent } from './componenti/deposito/deposita-assegni/deposita-assegni.component';
+import { SaldoServiceService } from './servizi/saldo-service.service';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    EstrattoContoComponent,
+    PrelievoComponent,
+    DepositoComponent,
+    DepositaContantiComponent,
+    DepositaAssegniComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
+  ],
+  providers: [SaldoServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
